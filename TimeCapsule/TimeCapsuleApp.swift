36e03ad5795692550f3a82f2c3501e4438716349
Shahira Jasmine Yahaya <shahirajasminee@gmail.com>
2025-03-14 19:05:25 +1100

    Initial Commit

---
+++ TimeCapsule/TimeCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCapsuleApp.swift
+//  TimeCapsule
+//
+//  Created by Shahira Jasmine on 14/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
